edd354b3bb4089be98f7a207781926c1eeb70b27
Ivan Camilo Triviño López <ivancamilotrivinolopez@MacBook-Pro-de-Ivan.local>
2020-04-06 16:39:18 -0500

    Add Product and Category models to the admin site.

--- omni_test/products/admin.py
+++ omni_test/products/admin.py
@@ -1,3 +1,9 @@
+# Django imports
 from django.contrib import admin
 
-# Register your models here.
+# Models imports
+from omni_test.products.models import Product, Category
+
+
+admin.site.register(Category)
+admin.site.register(Product)
